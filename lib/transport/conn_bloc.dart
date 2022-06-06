b60625592d1d6d48026d4cda52df9383ba6f8d0d
oringik <zikalol2@mail.ru>
2022-06-06 05:18:15 +0300

    message length buffer with little endian notation

--- lib/transport/conn_bloc.dart
+++ lib/transport/conn_bloc.dart
@@ -105,9 +105,8 @@ class TcpBloc {
         message = event.message;
       }
 
-      Uint8List messageWithLength = Uint8List(2 + message.length);
-      messageWithLength[0] = message.length;
-      messageWithLength[1] = message.length >> 8;
+      Uint8List messageWithLength = Uint8List(2 + message.length)
+        ..buffer.asByteData().setUint16(0, message.length, Endian.little);
 
       for (var i = 0; i < message.length; i++) {
         messageWithLength[i + 2] = message[i];
