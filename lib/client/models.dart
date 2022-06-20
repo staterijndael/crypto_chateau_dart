1b8546ecdcf4f95aab5511f119af19efeba1739b
oringik <zikalol2@mail.ru>
2022-06-21 02:39:58 +0300

    key expansion modify

--- lib/client/models.dart
+++ lib/client/models.dart
@@ -1,16 +1,23 @@
 import 'dart:typed_data';
 
-abstract class Request{
+abstract class Request {
   Uint8List Marshal();
 }
 
-class GetUserRequest extends Request{
+class GetUserRequest extends Request {
   final int userID;
 
   GetUserRequest({required this.userID});
 
-  Uint8List Marshal(){
-    String marshalStr = "GetUserRequest# UserID: $userID";
+  Uint8List Marshal() {
+    var buffer = Uint8List(8).buffer;
+    var bdata = ByteData.view(buffer);
+    bdata.setUint64(0, userID);
+
+    var bDataArr =
+        bdata.buffer.asUint8List(bdata.offsetInBytes, bdata.lengthInBytes);
+
+    String marshalStr = "GetUserRequest# UserID: $bDataArr";
 
     Uint8List data = Uint8List.fromList(marshalStr.codeUnits);
 
@@ -18,18 +25,18 @@ class GetUserRequest extends Request{
   }
 }
 
-abstract class Response{
+abstract class Response {
   Unmarshal(Map<String, Uint8List> params);
 }
 
-class GetUserResponse extends Response{
+class GetUserResponse extends Response {
   String? userName;
 
-  GetUserResponse([String? userName]){
+  GetUserResponse([String? userName]) {
     this.userName = userName!;
   }
 
-  Unmarshal(Map<String, Uint8List> params){
-      userName = String.fromCharCodes(params["UserName"]!);
+  Unmarshal(Map<String, Uint8List> params) {
+    userName = String.fromCharCodes(params["UserName"]!);
   }
 }
