f09e0b23a00fc286f71355f3dcabffecb40a6dba
oringik <zikalol2@mail.ru>
2022-07-15 18:05:59 +0300

    fix close bracket end of the string

--- lib/client/models.dart
+++ lib/client/models.dart
@@ -92,7 +92,6 @@ class GetUserResponse extends Message {
   }
 
   Unmarshal(Map<String, Uint8List> params) {
-    print(utf8.decode(params["User"]!));
     Map<String, Uint8List> userParams = getParams(params["User"]!);
     user.Unmarshal(userParams);
   }
