f739b799e3dc370e1b2a076892d04cf1f4f823a7
d.giniiatullin <d.giniiatullin@pointpay.io>
2023-01-31 17:45:49 +0400

    implement first where method in conn

--- lib/transport/conn.dart
+++ lib/transport/conn.dart
@@ -103,8 +103,7 @@ class Conn implements Socket {
   Stream<Uint8List> asBroadcastStream(
       {void Function(StreamSubscription<Uint8List> subscription)? onListen,
       void Function(StreamSubscription<Uint8List> subscription)? onCancel}) {
-    // TODO: implement asBroadcastStream
-    throw UnimplementedError();
+    return asBroadcastStream(onListen: onListen, onCancel: onCancel);
   }
 
   @override
