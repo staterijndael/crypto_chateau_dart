bb854244a76ec1f29cbf45fafed14fa85f590efd
RusOverLord <46760627+RusOverLord@users.noreply.github.com>
2023-03-04 14:59:15 +0300

    fix export

--- lib/crypto_chateau_dart.dart
+++ lib/crypto_chateau_dart.dart
@@ -1,5 +1,6 @@
 export 'package:crypto_chateau_dart/client/models.dart';
 export 'package:crypto_chateau_dart/client/conv.dart';
 export 'package:crypto_chateau_dart/transport/connection/connection.dart';
+export 'package:crypto_chateau_dart/transport/connection/peer.dart';
 export 'package:crypto_chateau_dart/client/binary_iterator.dart';
 export 'package:crypto_chateau_dart/transport/handler.dart';
